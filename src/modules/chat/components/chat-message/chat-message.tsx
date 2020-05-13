0ea928e7fea6a571fcb5ab823dd73feda8559391
Dominik Grzedzielski <gie.dominik@gmail.com>
2020-05-13 18:05:24 +0200

    Add tooltips, decrease messages size

--- src/modules/chat/components/chat-message/chat-message.tsx
+++ src/modules/chat/components/chat-message/chat-message.tsx
@@ -12,6 +12,7 @@ import {
 import { useAuth } from 'modules/auth/auth-context';
 import { Message } from 'modules/chat/types';
 import './chat-message.scss';
+import Tooltip from '@material-ui/core/Tooltip';
 
 type ChatMessageProps = {
   message: Message;
@@ -46,26 +47,30 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
             'chat-message--right': isSentByCurrentUser,
           })}
         >
-          <ListItemAvatar>
-            <Avatar
-              src={sender.avatarUrl || ''}
-              className="chat-message__avatar"
-            >
-              {sender.displayName.split('')[0]}
-            </Avatar>
-          </ListItemAvatar>
-          <Typography
-            variant="caption"
-            className="chat-message__created-at"
-            color="textSecondary"
+          <Tooltip title={sender.displayName} enterTouchDelay={0}>
+            <ListItemAvatar className="chat-message__avatar-container">
+              <Avatar
+                src={sender.avatarUrl || ''}
+                className="chat-message__avatar"
+              >
+                {sender.displayName.split('')[0]}
+              </Avatar>
+            </ListItemAvatar>
+          </Tooltip>
+          <Tooltip
+            enterTouchDelay={0}
+            title={
+              <Typography variant="caption" color="textSecondary">
+                <time dateTime={createdAt.toLocaleTimeString()}>
+                  {formattedCreatedAt}
+                </time>
+              </Typography>
+            }
           >
-            <time dateTime={createdAt.toLocaleTimeString()}>
-              {formattedCreatedAt}
-            </time>
-          </Typography>
-          <Typography variant="body2" className="chat-message__content">
-            {content}
-          </Typography>
+            <Typography variant="body2" className="chat-message__content">
+              {content}
+            </Typography>
+          </Tooltip>
         </div>
       </ListItem>
     </>
